9df639819d296f41f7348e54320e3bb5b967d50f
Navin <navint@metropolia.fi>
2026-09-24 11:25:53 +0300

    This is iteration1 backend

--- backend/api-server-starter/app.js
+++ backend/api-server-starter/app.js
@@ -1,7 +1,9 @@
+
 require('dotenv').config()
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const userRouter = require("./routes/userRouter")
 // const userRouter = require("./routes/userRouter");
 const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
@@ -15,7 +17,7 @@ app.use(morgan("dev"));
 connectDB();
 
 // Use the userRouter for all /users routes
-// app.use("/api/users", userRouter);
+app.use("/api/users", userRouter);
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
